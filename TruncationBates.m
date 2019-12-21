107f6169b8f6648eb3eee89051fecab1b74325f6
Philip Douw <47745437+PhilipDouw@users.noreply.github.com>
2019-12-21 15:24:38 +0100

    Delete TruncationBates.m

--- TruncationBates.m
+++
@@ -1,8 +0,0 @@
-function [TruncError,TruncErrPercentage] = TruncationBates(S, K_min, K_max, r, T, sigma)
-      
-TruncError = -2/T * exp(r*T) * [(integral(@(K) putBates(S, K, r, T, sigma)/K^2,0,K_min,'ArrayValued',true)) + ...
-                   +(integral(@(K) callBates(S, K, r, T, sigma)/K^2,K_max,inf,'ArrayValued',true))]
-
-TruncErrPercentage = TruncError / sigma
-
-end
