581502f449db68aa2622ff8f0cd70949bb9c4a53
DavidDiserens <47818260+DavidDiserens@users.noreply.github.com>
2019-12-31 12:10:48 +0100

    Delete ExpansionBates.m

--- ExpansionBates.m
+++
@@ -1,7 +0,0 @@
-function [Matrix, ExpansionError] = ExpansionBates(S , K_min, K_max, interval, r, T, sigma)
-    
-    [Matrix, F_zero, K_zero] = PutCallBates(S , K_min, K_max, interval, r, T, sigma);
-    
-    ExpansionError = 2/T * [[(F_zero / K_zero - 1) - 1/2*(F_zero / K_zero - 1)^2] - log(F_zero / K_zero)];
-    
-end
